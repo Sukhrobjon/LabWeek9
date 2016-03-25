d07024e8dd98db9ac39ef50cad5b1d574221a67b
Sukhrobjon <gholibboev@gmail.com>
2016-03-24 19:33:48 -0700

    Initial Commit

---
+++ Week9Tests/Week9Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Week9Tests.swift
+//  Week9Tests
+//
+//  Created by Sukhrobjon Golibboev on 3/24/16.
+//  Copyright © 2016 Sukhrobjon Golibboev. All rights reserved.
+//
+
+import XCTest
+@testable import Week9
+
+class Week9Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
